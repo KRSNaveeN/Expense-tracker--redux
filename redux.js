64f80219e20daa5f7d7fb12804412340fdea1b73
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2024-01-09 17:35:46 +0530

    Add files via upload

--- redux.js
+++ redux.js
@@ -2,19 +2,31 @@ const redux = require('redux');
 
 const counterReducer = (state={counter : 0},action)=>{
 
-    if(action.type == "increment")
+    if(action.type === "increment")
     {
         return {
             counter : state.counter+1
         }
     }
 
-    else if(action.type == 'decrement')
+     if(action.type === 'decrement')
     {
         return {
             counter : state.counter-1
         }
     }
+     if(action.type === 'incrementby2')
+     {
+        return {
+            counter : state.counter+2
+        }
+     }
+      if(action.type === 'decrementby2')
+     {
+        return {
+            counter : state.counter-2
+        }
+     }
     
 }
 // reducer function should be a pure function
@@ -29,10 +41,17 @@ const counterSubscriber = ()=>{
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({type:"increment"})
-store.dispatch({type:"increment"})
-store.dispatch({type:"increment"})
-store.dispatch({type:"increment"})
-store.dispatch({type:"increment"})
 
-store.dispatch({type:'decrement'});
+store.dispatch({type : 'decrementby2'});
+store.dispatch({type : 'incrementby2'});
+store.dispatch({type : 'incrementby2'});
+store.dispatch({type : 'incrementby2'});
+
+// store.dispatch({type:"increment"})
+// store.dispatch({type:"increment"})
+// store.dispatch({type:"increment"})
+// store.dispatch({type:"increment"})
+// store.dispatch({type:"increment"})
+
+// store.dispatch({type:'decrement'});
+
